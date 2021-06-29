cff9f6247edd965fe997310755832e58c877ce44
Fred Israel <fredpublico@gmail.com>
2021-06-29 17:06:42 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SHUTUP
+
+Sometimes you just can't mute python warnings. Use this library to solve this.
+
+On the begging of your code (before you import other things) you should add this line:
+
+```python
+import shutup; shutup.please()
+```
+
+That's it.
---
+++ shutup/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.0'
+
+def please():
+    import warnings
+    please._orig_warning = warnings.warn
+    def warn(message:str, category:str='', stacklevel:int=1, source:str=''): # need hints to work with pytorch
+        pass
+    warnings.warn = warn
