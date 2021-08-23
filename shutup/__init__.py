aab0fd01f898951a6b914683623af04d3f5144d7
Fred Israel <fredpublico@gmail.com>
2021-08-23 11:45:25 -0300

    Remove bare except

--- shutup/__init__.py
+++ shutup/__init__.py
@@ -1,11 +1,10 @@
-from importlib.metadata import version
-try: __version__ = version(__name__)
-except: pass
-del version
+from importlib.metadata import version as _version
+try: __version__ = _version(__name__)
+except Exception: pass
 
 def please():
     import warnings
     please._orig_warning = warnings.warn
     def warn(message:str, category:str='', stacklevel:int=1, source:str=''): # need hints to work with pytorch
-        pass
+        pass # In the future, we can implement filters here. For now, just mute everything.
     warnings.warn = warn
